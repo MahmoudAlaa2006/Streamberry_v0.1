459227ee3adf03284bd42e9d6e4ca97704629b72
Mahmoud <mahalaabatman98@gmail.com>
2025-07-13 15:06:13 +0300

    adding Login, Regester, Contact us, and Movies folders

---
+++ login_registration_contactus/contactus.js
@@ -0,0 +1,8 @@
+let login = document.getElementById("Login");
+login.onclick=function(){
+    window.location.href='login.html';
+}
+let home = document.getElementById("Home");
+home.onclick=function(){
+    window.location.href='../docs/index.html';
+}
---
+++ login_registration_contactus/register.js
@@ -0,0 +1,12 @@
+let login = document.getElementById("Login");
+login.onclick=function(){
+    window.location.href='login.html';
+}
+let home = document.getElementById("Home");
+home.onclick=function(){
+    window.location.href='../docs/index.html';
+}
+let contact_us = document.getElementById("Contact-Us");
+contact_us.onclick=function(){
+    window.location.href='contactus.html';
+}
