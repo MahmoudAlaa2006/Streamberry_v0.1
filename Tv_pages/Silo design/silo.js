639477eb79f4863619a225662969fffee7aea0b3
Mahmoud <mahalaabatman98@gmail.com>
2025-07-13 15:07:14 +0300

    adding Tv shows folder

---
+++ Tv_pages/Silo design/silo.js
@@ -0,0 +1,16 @@
+let login = document.getElementById("Login");
+login.onclick=function(){
+    window.location.href='../../login_registration_contactus/login.html';
+}
+let home1 = document.getElementById("Home1");
+home1.onclick=function(){
+    window.location.href='../../docs/index.html';
+}
+let home2 = document.getElementById("Home2");
+home2.onclick=function(){
+    window.location.href='../../docs/index.html';
+}
+let contact_us = document.getElementById("Contact-Us");
+contact_us.onclick=function(){
+    window.open('../../login_registration_contactus/contactus.html');
+}
